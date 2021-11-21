b5520e6e136831b6ef67d1c387128a7d64fcca79
Manali Shetye <manalis826@gmail.com>
2021-11-22 00:20:12 +0530

    setup of emit and listen events

--- static/js/index.js
+++ static/js/index.js
@@ -1,1 +1,31 @@
+//set connection
 const socket = io.connect('http://localhost:8080');
+
+const message = document.getElementById('message');
+const handle = document.getElementById('handle');
+const sendBtn = document.getElementById('sendBtn');
+const output = document.getElementById('output');
+const feedback = document.getElementById('feedback');
+
+//emit events
+sendBtn.addEventListener('click', ()=>{
+    socket.emit('chat', {
+        message:message.value,
+        handle: handle.value
+    });
+    message.value = "";
+});
+
+message.addEventListener('keypress',()=>{
+    socket.emit('typing', handle.value);
+});
+
+//listen for events
+socket.on('display', (data)=>{
+    feedback.innerHTML = ``;
+    output.innerHTML += `<p><strong>${data.handle}: </strong>${data.message}</p>`;
+});
+
+socket.on('typingView', (data)=>{
+    feedback.innerHTML = `<p><em>${data} is typing a message...</em></p>`;
+});
